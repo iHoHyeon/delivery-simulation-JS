d2a9dca661ebf21cfa4f96fce237c408383fe98f
iHoHyeon <hyunee169@naver.com>
2021-12-11 17:15:37 +0900

    feat: myEventEmitter eventName type 조건 추가

--- src/javascripts/myEventEmitter.js
+++ src/javascripts/myEventEmitter.js
@@ -4,6 +4,7 @@ export default class MyEventEmitter {
   }
 
   on(eventName, listener) {
+    if (typeof eventName !== 'string' && typeof eventName !== 'symbol') throw Error('event name should be string or symbol type');
     if (typeof listener !== 'function') throw Error('listener should be function');
     this.onListner.set(eventName, [...(this.onListner.get(eventName) ?? []), listener]);
     this.emit('newListner');

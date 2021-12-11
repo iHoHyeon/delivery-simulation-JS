8a0ba1f039189dd7d152bcd4d6d1bc4aa72feed2
iHoHyeon <hyunee169@naver.com>
2021-12-12 02:23:08 +0900

    etc: dashboard 임시 console.log 출력 테스트

--- src/javascripts/class/dashboard.js
+++ src/javascripts/class/dashboard.js
@@ -2,6 +2,12 @@
   DashBoards는 메뉴 제작 현황을 주기적으로 화면에 출력
 */
 
-class DashBoard {
-  constructor() {}
+export default class DashBoard {
+  constructor(eventEmitter) {
+    this.eventEmitter = eventEmitter;
+
+    eventEmitter.on('finishOrder', (order) => console.log('finishOrder', order));
+    eventEmitter.on('startMaking', (customer, menu, count, name) => console.log('startMaking', customer, menu, count, `by ${name}`));
+    eventEmitter.on('finishMaking', (customer, menu, count, name) => console.log('finishMaking', customer, menu, count, `by ${name}`));
+  }
 }

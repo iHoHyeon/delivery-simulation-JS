c1d74c8f0ef64dcf26ed4170d4957be3a7c356c4
iHoHyeon <hyunee169@naver.com>
2021-12-11 19:09:56 +0900

    temp: 동작 확인을 위한 index.js 코드

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,16 @@
-const init = 'init';
+import MyEventEmitter from './class/myEventEmitter.js';
+import POS from './class/pos.js';
+import Manager from './class/manager.js';
+import Barista from './class/barista.js';
+
+const eventEmitter = new MyEventEmitter();
+eventEmitter.on('addOrder', () => console.log('addOrder'));
+
+const pos = new POS(eventEmitter);
+const manager = new Manager(eventEmitter, pos);
+const barista1 = new Barista(eventEmitter);
+const barista2 = new Barista(eventEmitter);
+manager.hireBaristas(barista1, barista2, barista1, '123');
+
+pos.addOrder({ A: { 아메리카노: 1, 바닐라라떼: 2 } });
+pos.addOrder({ B: { 아메리카노: 2 } });

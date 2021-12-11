fba3319e80c9ad5f7fd5d88a97e17e3fee4b8677
iHoHyeon <hyunee169@naver.com>
2021-12-12 02:26:11 +0900

    etc: console에서 테스트를 위한 코드 작성

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,15 +2,26 @@ import MyEventEmitter from './class/myEventEmitter.js';
 import POS from './class/pos.js';
 import Manager from './class/manager.js';
 import Barista from './class/barista.js';
+import DashBoard from './class/dashboard.js';
 
 const eventEmitter = new MyEventEmitter();
-eventEmitter.on('addOrder', () => console.log('addOrder'));
 
 const pos = new POS(eventEmitter);
+const dashBoard = new DashBoard(eventEmitter);
 const manager = new Manager(eventEmitter, pos);
-const barista1 = new Barista(eventEmitter);
-const barista2 = new Barista(eventEmitter);
-manager.hireBaristas(barista1, barista2, barista1, '123');
+const barista1 = new Barista(eventEmitter, '바리스타1');
+const barista2 = new Barista(eventEmitter, '바리스타2');
+const barista3 = new Barista(eventEmitter, '바리스타3');
+const barista4 = new Barista(eventEmitter, '바리스타4');
+const barista5 = new Barista(eventEmitter, '바리스타5');
+manager.hireBaristas(barista1, barista2, barista3, barista4, barista5);
 
-pos.addOrder({ A: { 아메리카노: 1, 바닐라라떼: 2 } });
-pos.addOrder({ B: { 아메리카노: 2 } });
+const testOrder = () => {
+  pos.addOrder({ A: { 아메리카노: 2, 바닐라라떼: 1, 허니브레드: 2 } });
+  pos.addOrder({ B: { 아메리카노: 4 } });
+  pos.addOrder({ C: { 아메리카노: 2 } });
+  pos.addOrder({ D: { 아메리카노: 1 } });
+  pos.addOrder({ E: { 허니브레드: 1 } });
+};
+
+testOrder();

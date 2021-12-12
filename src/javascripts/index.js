84708941d85646757c215cdf756e4204d9859a07
iHoHyeon <hyunee169@naver.com>
2021-12-12 23:49:09 +0900

    etc: 지속적 감시 확인을 위한 바리스타 2명으로 수정

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -11,18 +11,7 @@ const dashBoard = new DashBoard(eventEmitter);
 const manager = new Manager(eventEmitter, pos);
 const barista1 = new Barista(eventEmitter, '바리스타1');
 const barista2 = new Barista(eventEmitter, '바리스타2');
-const barista3 = new Barista(eventEmitter, '바리스타3');
-const barista4 = new Barista(eventEmitter, '바리스타4');
-const barista5 = new Barista(eventEmitter, '바리스타5');
-manager.hireBaristas(barista1, barista2, barista3, barista4, barista5);
-
-const testOrder = () => {
-  pos.addOrder({ A: { 아메리카노: 2, 바닐라라떼: 1, 허니브레드: 2 } });
-  pos.addOrder({ B: { 아메리카노: 4 } });
-  pos.addOrder({ C: { 아메리카노: 2 } });
-  pos.addOrder({ D: { 아메리카노: 1 } });
-  pos.addOrder({ E: { 허니브레드: 1 } });
-};
+manager.hireBaristas(barista1, barista2);
 
 const setLayout = () => {
   const container = document.querySelector('.container');

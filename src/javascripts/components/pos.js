73ac79c11dd6e1895d1c0a2a6cd9960a321421ea
iHoHyeon <hyunee169@naver.com>
2021-12-12 19:30:55 +0900

    feat: pos 엘리먼트 이벤트 세팅

--- src/javascripts/components/pos.js
+++ src/javascripts/components/pos.js
@@ -23,6 +23,47 @@ export default class POS {
     `;
   }
 
+  setDOMEvent() {
+    const orderButton = document.querySelector('button.order');
+    const orderCountList = document.querySelectorAll('.menu input');
+    const customerNameInput = document.querySelector('.customer input');
+
+    const checkIsNum = (event) => {
+      event.currentTarget.value = event.currentTarget.value.replaceAll(/[^0-9.]/g, '');
+    };
+
+    const checkIsDisabled = () => {
+      const check = [...orderCountList].some((input) => input.value) && customerNameInput.value;
+      orderButton.disabled = !check;
+    };
+
+    const clearOrder = () => {
+      [...orderCountList, customerNameInput].forEach((input) => (input.value = 0));
+    };
+
+    const submitOrder = () => {
+      const [americano, vanillaLatte, honeyBead] = Array.from(orderCountList).map((input) => input.value);
+      const customerName = customerNameInput.value;
+      const order = {};
+
+      if (americano) order[customerName] = { ...order[customerName], 아메리카노: americano };
+      if (vanillaLatte) order[customerName] = { ...order[customerName], 바닐라라떼: vanillaLatte };
+      if (honeyBead) order[customerName] = { ...order[customerName], 허니브레드: honeyBead };
+
+      this.addOrder(order);
+      clearOrder();
+    };
+
+    orderCountList.forEach((input) => {
+      input.addEventListener('input', checkIsNum);
+      input.addEventListener('input', checkIsDisabled);
+    });
+
+    customerNameInput.addEventListener('input', checkIsDisabled);
+
+    orderButton.addEventListener('click', submitOrder);
+  }
+
   addOrder(order) {
     this.orderQueue.push(order);
     this.eventEmitter.emit('addOrder');

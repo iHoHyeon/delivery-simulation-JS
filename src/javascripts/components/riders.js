b463f668a00de429a5b6682d1d58f32eba3a6fb4
iHoHyeon <hyunee169@naver.com>
2021-12-17 14:56:04 +0900

    fix: 배달 시간 5분 -> 10분으로 수정

--- src/javascripts/components/riders.js
+++ src/javascripts/components/riders.js
@@ -9,7 +9,7 @@ export default class Rider {
   async startRiding(order) {
     this.eventEmitter.emit('startRiding', order, this.name);
     this.nowRiding = true;
-    await new Promise((res) => setTimeout(() => res(), 2000 * 5));
+    await new Promise((res) => setTimeout(() => res(), 2000 * 10));
     return this.finishRiding(order);
   }
 
